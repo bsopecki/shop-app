4eb6ed041a0f5e8ed4435b183055ced55ed7045d
Bartłomiej Sopecki <bartlomiej.sopecki@mobica.com>
2021-04-19 14:36:12 +0200

    add navigation to the header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
                 <Container>
                     <Navbar.Brand href="/">ProShop</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
                             <Nav.Link href="/cart"><i className='fas fa-shopping-cart'>Cart</i></Nav.Link>
