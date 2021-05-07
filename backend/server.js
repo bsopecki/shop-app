dedc5121af7f350b786498983d45de6db0b2bdd1
Bartłomiej Sopecki <bartlomiej.sopecki@mobica.com>
2021-05-07 14:29:57 +0200

    install mongoDB and connect with server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -24,4 +28,4 @@ const DEV = process.env.NODE_ENV
 
 
 
-app.listen(PORT, console.log(`Server running in ${DEV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${DEV} mode on port ${PORT}`.yellow.bold))
